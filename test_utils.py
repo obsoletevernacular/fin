958f72eac85cb9ef75e524dcb6df1049c3266920
Eli Cash <obsoletefernacular@gmail.com>
2017-11-10 07:32:37 -0800

    remove unittest from test_utils
    - remove unittest form test_utils
    - replace unittest tests with pytest tests
    :

--- test_utils.py
+++ test_utils.py
@@ -1,38 +1,50 @@
 """Unit tests for utils.py."""
-import unittest
+import pytest
+import os
+
 import utils
 from report import Transaction
 
 
-class UtilsTest(unittest.TestCase):
-    """Test utility functions."""
+@pytest.fixture()
+def csvload_happy():
+    """Test fixture for csvload - happ path."""
+    csvload_happy = os.path.join(os.getcwd(), "testfiles/csvload_happy.test")
+    ts = []
+    with open(csvload_happy, "r") as f:
+        ts = utils.csvload(f)
+    f.closed
+    return ts
 
-    def test_csvload_happy(self):
-        """Test csvload happy path."""
-        ts = []
-        with open("testfiles/csvload_happy.test", "r") as f:
-            ts = utils.csvload(f)
-        f.closed
-        self.assertEqual(2, len(ts))
-        for t in ts:
-            self.assertIsInstance(t, Transaction)
+def test_csvload_happy_size(csvload_happy):
+    assert 2 == len(csvload_happy)
+    """Test csvload happy path."""
 
-    def test_csvload_unopened_file(self):
-        """Test csvload with an unopened file."""
-        self.assertRaises(utils.InvalidCSV, utils.csvload,
-                          "testfiles/csvload_happy.test")
 
-    def test_csvload_notacsv(self):
-        """Test csvload with non-csv file."""
-        self.assertRaises(utils.InvalidCSV, utils.csvload,
-                          "testfiles/csvload_notacsv.test")
+def test_csvload_happy_transaction(csvload_happy):
+    for t in csvload_happy:
+        assert isinstance(t, Transaction)
 
-    def test_csvload_invalidfieldscsv(self):
-        """Test csvload with a csv file with invalid field headers."""
-        self.assertRaises(utils.InvalidCSV, utils.csvload,
-                          "testfiles/csvload_invalidfieldscsv.test")
 
-    def test_csvload_nodescriptioncsv(self):
-        """Test csvload with a csv file with poorly formatted rows."""
-        self.assertRaises(utils.InvalidCSV, utils.csvload,
-                          "testfiles/csvload_nodescriptioncsv.test")
+def test_csvload_unopened_file():
+    """Test csvload with an unopened file."""
+    pytest.raises(utils.InvalidCSV, utils.csvload,
+                      "testfiles/csvload_happy.test")
+
+
+def test_csvload_notacsv():
+    """Test csvload with non-csv file."""
+    pytest.raises(utils.InvalidCSV, utils.csvload,
+                      "testfiles/csvload_notacsv.test")
+
+
+def test_csvload_invalidfieldscsv():
+    """Test csvload with a csv file with invalid field headers."""
+    pytest.raises(utils.InvalidCSV, utils.csvload,
+                      "testfiles/csvload_invalidfieldscsv.test")
+
+
+def test_csvload_nodescriptioncsv():
+    """Test csvload with a csv file with poorly formatted rows."""
+    pytest.raises(utils.InvalidCSV, utils.csvload,
+                      "testfiles/csvload_nodescriptioncsv.test")
